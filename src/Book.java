14cdc76417c6ffa854fb7a93d9219fcf489f0085
Кирик <kmaibo@mail.ru>
2025-09-18 22:57:05 +0300

    homework completed

---
+++ src/Book.java
@@ -0,0 +1,24 @@
+public class Book {
+    private String title;
+    private int year;
+
+    public Book(String title, int year) {
+        this.title = title;
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+    public Author getAuthorName(){
+        return this.getAuthorName();
+    }
+
+    public int getYear() {
+        return this.year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+}
