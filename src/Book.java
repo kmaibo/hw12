3f4ef7f97d862642de35f39391190acd60d9f258
Кирик <kmaibo@mail.ru>
2025-09-21 14:50:32 +0300

    homework completed

--- src/Book.java
+++ src/Book.java
@@ -1,11 +1,16 @@
 public class Book {
     private String title;
     private int year;
+    private Author author;
 
-    public Book(String title, int year) {
+    public Book(String title, int year, Author author) {
         this.title = title;
         this.year = year;
+        this.author = author;
     }
+
+//    public Book(int year, Author authorOne) {
+//    }
 
     public String getTitle() {
         return this.title;
--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         Author authorOne = new Author("Федор", "Достоевский");
         Author authorTwo = new Author("Лев", "Толстой");
-        Book bookOne = new Book("Идиот", 1886);
-        Book bookTwo = new Book("Война и мир", 1869);
+        Book bookOne = new Book("Идиот", 1886, authorOne);
+        Book bookTwo = new Book("Война и мир", 1869, authorTwo);
         bookOne.setYear(1887);
         System.out.println("Книга: " + bookOne.getTitle() + ". Автор: " + authorOne.getFirstName() + authorOne.getLastName() + ". Год публикации: " + bookOne.getYear());
         System.out.println("Книга: " + bookTwo.getTitle() + ". Автор: " + authorTwo.getFirstName() + authorTwo.getLastName() + ". Год публикации: " + bookTwo.getYear());
